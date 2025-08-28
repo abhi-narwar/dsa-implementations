033a374a143d016fc06b297358b3905bb090dcde
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2025-08-28 08:33:27 +0530

    leetcode 49 group_anagrams

--- Leetcode Questions/group_anagrams.py
+++ Leetcode Questions/group_anagrams.py
@@ -1,11 +1,28 @@
-from collections import defaultdict
-
 class Solution:
-    def groupAnagrams(self, strs):
-        anagrams = defaultdict(list)  # key: sorted word, value: list of anagrams
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        # HashMap to store sorted string as key, and list of anagrams as value
+        hashMap = {}
 
-        for word in strs:
-            key = ''.join(sorted(word))  # sort the letters to form key
-            anagrams[key].append(word)
+        # Loop through each word in the input list
+        for s in strs:
+            # Convert word to char array, sort it, then join back into string
+            # Example: "eat" -> ['a','e','t'] -> "aet"
+            charArray = sorted(s)
+            sortedStr = "".join(charArray)
 
-        return list(anagrams.values())
+            # If the sorted word already exists in map, append current word
+            if sortedStr in hashMap:
+                listSubAnswer = hashMap[sortedStr]
+                listSubAnswer.append(s)
+            else:
+                # Otherwise, create a new list with this word
+                listSubAnswer = [s]
+                hashMap[sortedStr] = listSubAnswer
+
+        # Collect all grouped anagrams from the hashmap
+        listAnswer = []
+        for value in hashMap.values():
+            listAnswer.append(value)
+
+        # Return the final list of lists
+        return listAnswer
