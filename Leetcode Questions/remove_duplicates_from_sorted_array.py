9347ad0183a5b6edaa0794dc81d2c98fda36907e
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2025-08-30 11:32:34 +0530

    added code

---
+++ Leetcode Questions/remove_duplicates_from_sorted_array.py
@@ -0,0 +1,17 @@
+from typing import List
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n=len(nums)
+        i=1
+        for j in range(1,n):
+            if nums[j] != nums[j-1]:
+                nums[i]=nums[j]
+                i+=1
+        return i
+    
+
+
+
+        
+
+        
