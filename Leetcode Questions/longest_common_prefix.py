0758076af1a7514af8047c6ead6e2a6da0268c75
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2025-09-02 12:56:05 +0530

    added code

--- Leetcode Questions/longest_common_prefix.py
+++ Leetcode Questions/longest_common_prefix.py
@@ -1,19 +1,8 @@
 from typing import List
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        # If only one string, return it directly
-        if len(strs) == 1:
-            return strs[0]
-
-        # Take the first string as initial prefix
-        prefix = strs[0]
-
-        # Compare prefix with each string in the list
-        for s in strs[1:]:
-            # While current string doesn't start with prefix, reduce prefix
-            while not s.startswith(prefix):
-                prefix = prefix[:-1]  # remove last character
-                if prefix == "":
-                    return ""  # no common prefix
-        
-        return prefix
+        smallest, largest = min(strs), max(strs)
+        for i, ch in enumerate(smallest):
+            if ch != largest[i]:
+                return smallest[:i]
+        return smallest
