98e73162498ba59bc17f0d745eaef43511b5f711
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2025-09-17 10:53:04 +0530

    added code

---
+++ Leetcode Questions/reverse_string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        st=0
+        end=len(s)-1
+        while st<end:
+            s[st],s[end]=s[end],s[st]
+            st+=1
+            end-=1
+            
+            
+        
