9f9d3678aacb0597208eeb961779595700c9f0c4
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2025-10-13 15:01:12 +0530

    added code

--- Leetcode Questions/sort_colors.py
+++ Leetcode Questions/sort_colors.py
@@ -1,17 +1,15 @@
 class Solution:
-    def sortColors(self, nums: list[int]) -> None:
-        n = len(nums)
-        l = 0
-        m = 0
-        h = n - 1
-
-        while m <= h:
-            if nums[m] == 2:
-                nums[m], nums[h] = nums[h], nums[m]
-                h -= 1
-            elif nums[m] == 0:
-                nums[l], nums[m] = nums[m], nums[l]
-                l += 1
-                m += 1
-            else:  # nums[m] == 1
-                m += 1
+    def sortColors(self, nums: List[int]) -> None:
+        n=len(nums)
+        l,m=0,0
+        h=n-1
+        while m<=h:
+            if nums[m]==2:
+                nums[m],nums[h]=nums[h],nums[m]
+                h-=1
+            elif nums[m]==0:
+                nums[l],nums[m]=nums[m],nums[l]
+                l+=1
+                m+=1
+            else:
+                m+=1
